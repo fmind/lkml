62a7fa15f2f8c1915013e8ab9d778ab041c44302
Freaxmind <freaxmind@users.noreply.github.com>
2016-11-17 11:53:34 +0100

    First commit

---
+++ lkml/spiders/mails.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+from functools import *
+import scrapy
+
+attr = lambda attribute, element: element.css("::attr({0})".format(attribute)).extract_first()
+text = lambda element: element.css("::text").extract_first()
+itemprop = partial(attr, "itemprop")
+
+class MailsSpider(CrawlSpider):
+    name = "mails"
+    allowed_domains = ["lkml.org"]
+    start_urls = ['https://lkml.org/lkml/']
+
+    rules = [
+        Rule(LinkExtractor(allow='/lkml/\d{4}/\d{1,2}/\d{1,2}/\d+'), callback='parse_mail'),
+        Rule(LinkExtractor(allow=['/lkml[0-9/]+']), follow=True),
+    ]
+
+    def parse_mail(self, response):
+        yield {itemprop(p): text(p) for p in response.css("*[itemprop]")}
